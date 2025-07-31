20cb71b708dbfad857035c1b578f519c9e70a81c
Igor <igor.soaresbarbosa@hotmail.com>
2025-07-31 13:55:30 -0300

    Parelell srw using taichi

--- models/srw.py
+++ models/srw.py
@@ -7,14 +7,25 @@ import taichi as ti
 - (1-q) is the probability of an downward step (-1)
 '''
 
-@ti.data_oriented
-def srw(k,q=0.5):
-    x = np.random.choice([-1,1],k, p=[1-q, q])
-    rw = sum(x)
-    return rw
+@ti.func
+def srw(n: ti.i32, q: ti.f64=0.5) -> ti.f64:
+    assert 0 < q and q < 1, "q must be in the open interval (0,1)"
+    pos = 0.0
+    for _ in range(n):
+        pos += 1.0 if ti.random() < q else -1.0
+    return pos
+
+@ti.kernel
+def main(n: ti.i32, q: ti.f64) -> float:
+    data = srw(n,q)
+    return data
 
 if __name__=="__main__":
-    ''' Example of usage '''
-    k = 15
-    q = 0.5
-    print(srw(k,q))
+    # Example of usage
+
+    ti.init(debug=True)
+    n = 10**10
+    q = 0.5 
+    result = main(n,q)
+
+    print(result)
