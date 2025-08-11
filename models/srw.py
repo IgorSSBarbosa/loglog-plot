f1ea0ca5820659591340bda04ef54c7dd7b7779f
Igor <igor.soaresbarbosa@hotmail.com>
2025-08-11 18:58:05 -0300

    use numpy srw

--- models/srw.py
+++ models/srw.py
@@ -1,5 +1,4 @@
 import numpy as np
-import taichi as ti
 
 '''  Defines a simple random walk, each step is +1 or -1
 - k is the number of steps
@@ -7,25 +6,12 @@ import taichi as ti
 - (1-q) is the probability of an downward step (-1)
 '''
 
-@ti.func
-def srw(n: ti.i32, q: ti.f64=0.5) -> ti.f64:
-    assert 0 < q and q < 1, "q must be in the open interval (0,1)"
-    pos = 0.0
-    for _ in range(n):
-        pos += 1.0 if ti.random() < q else -1.0
-    return pos
-
-@ti.kernel
-def main(n: ti.i32, q: ti.f64) -> float:
-    data = srw(n,q)
-    return data
+def srw(k,q=0.5):
+    x = np.random.choice([-1,1],k, p=[1-q, q])
+    rw = sum(x)
+    return np.abs(rw)
 
 if __name__=="__main__":
-    # Example of usage
-
-    ti.init(debug=True)
-    n = 10**10
-    q = 0.5 
-    result = main(n,q)
-
-    print(result)
+    ''' Example of usage '''
+    k = 15
+    print(srw(k))
